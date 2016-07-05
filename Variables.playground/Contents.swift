462d1181300eaa96491fd73bbbd9a40f30f024a6
sheafk <sheafureyking@gmail.com>
2016-07-05 13:25:39 -0400

    Done.

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,20 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Harry Potter"
+print(favoriteCharacter)
 
-let ultimateFavoriteCharacter = "Arya Stark"
+var ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
 ultimateFavoriteCharacter = "Daenerys Targaryen"
+
+var number = 3
+number = 4
+
+print(number)
+
+var otherNumber = 18
+otherNumber = number + otherNumber
+
+print(otherNumber)
